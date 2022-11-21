69d6315522c09bbe183dd20d77360f3cd29f99d0
KP_349 <KP_349@FANARIER>
2022-11-21 21:04:46 +0200

    Добавьте файлы проекта.

---
+++ Character data type/Program.cs
@@ -0,0 +1,21 @@
+﻿//Двухмерные массивы. Плотников Кирилл Андреевич 22-ИСП-2/2
+//№варианта:16; Средний уровень 
+//Введите массив символов из 11 элементов. Убрать лишние пробелы (более одного подряд).
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+char[] mas = new char[11];
+Random rand = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = (char)rand.Next(60, 128);
+    Console.Write(mas[i] + "");
+}
+for (int i = 0; i < mas.Length; i++)
+{
+    int z = (int)mas[i];
+    mas[i] = (char)z;
+    Console.Write(mas[i] + "");
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
